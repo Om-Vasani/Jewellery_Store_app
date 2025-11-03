6de573569d852eeb3714ff416f907d84c9b11072
Om-Vasani <omvasani63@gmail.com>
2025-11-03 19:31:44 +0530

    Remove unnecessary configuration options

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  base: './',
-  publicDir: 'public',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
